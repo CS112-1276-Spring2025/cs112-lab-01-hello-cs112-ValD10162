26de22ee133e4e31bc2d736271b08863b4ec397b
Valentine <67397091+ValD10162@users.noreply.github.com>
2025-01-23 23:37:24 +0000

    finished part 1 and 2

--- Main.java
+++ Main.java
@@ -2,6 +2,24 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello world! from Valentine :)");
+        System.out.println("Welcome to CS 112!");
+        Main.doNothing();
+        Main.randomNum();
+
+
+    }
+
+    public static void doNothing()
+    {
+        //idk it does nothing
+    }
+
+    public static double randomNum()
+    {
+        double random = Math.random();
+        int wholeNum = (int)(random * 10) * 1;
+        System.out.println(wholeNum);
+
+        return random;
     }
 }
